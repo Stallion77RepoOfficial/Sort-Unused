e17b71f31aa9b62fc94f213578367a496d79e62a
Stallion77RepoOfficial <149940678+Stallion77RepoOfficial@users.noreply.github.com>
2023-12-13 12:30:52 +0300

    Update sort-unused.py

--- sort-unused.py
+++ sort-unused.py
@@ -1,41 +1,53 @@
 import os
 import time
 
-def eski_dosyalari_listele(dizin):
+def dosyalari_tara(dizin, max_derinlik=0, mevcut_derinlik=0):
     # Mevcut zamanı al
     suanki_zaman = time.time()
+    dosyalar_ve_klasorler = []
 
-    # Dizindeki dosya ve klasörleri yürü
-    dosyalar = [os.path.join(dizin, f) for f in os.listdir(dizin)]
-    
-    # Toplam dosya sayısı
-    toplam_dosya = len(dosyalar)
-    print(f"{toplam_dosya} dosya bulundu. Tarama başlıyor...")
+    try:
+        # Dizindeki dosya ve klasörleri yürü
+        with os.scandir(dizin) as it:
+            for giris in it:
+                if giris.is_file():
+                    # Son değiştirilme zamanını al ve güne çevir
+                    son_degistirme = os.path.getmtime(giris.path)
+                    gun_farki = (suanki_zaman - son_degistirme) / 86400
+                    if gun_farki > esik_gun:
+                        dosyalar_ve_klasorler.append(giris.path)
+                elif giris.is_dir():
+                    # Klasörler için ayrı bir kontrol
+                    dosyalar_ve_klasorler.append(giris.path)
+                    # Eğer derinlik sınırlaması varsa ve mevcut derinlik max'e ulaşmamışsa, klasör içine dal
+                    if max_derinlik == 0 or mevcut_derinlik < max_derinlik:
+                        dosyalar_ve_klasorler.extend(dosyalari_tara(giris.path, max_derinlik, mevcut_derinlik + 1))
+    except PermissionError:
+        # Yetki hatası durumunda bu dizini atla
+        pass
 
-    # Taranan dosya sayısı
-    taranan_dosya = 0
+    return dosyalar_ve_klasorler
 
-    # Dosyaları listele
-    for dosya in dosyalar:
-        try:
-            # Son değiştirilme zamanını al
-            son_degistirme = os.path.getmtime(dosya)
-            gun_farki = (suanki_zaman - son_degistirme) / 86400  # saniyeyi güne çevir
-
-            # Eğer dosya 1 haftadan (yaklaşık 7 günden) eskiyse yazdır
-            if gun_farki > 7:
-                print(f"{dosya} en son {int(gun_farki)} gün önce değiştirilmiş.")
-        except OSError as e:
-            print(f"Hata: {e}")
-
-        taranan_dosya += 1
-        print(f"Tarama ilerlemesi: {taranan_dosya}/{toplam_dosya} dosya tarandı.", end='\r')
-
-    print("\nTarama tamamlandı.")
+# Tarama ilerlemesini göster
+def tarama_ilerlemesi(dosyalar, toplam_dosya):
+    for i, dosya in enumerate(dosyalar, start=1):
+        print(f"Tarama ilerlemesi: {i}/{toplam_dosya} | {dosya}")
+        time.sleep(0.1)  # İlerlemeyi gözlemlemek için küçük bir gecikme
 
 if __name__ == "__main__":
     dizin = input("Lütfen taramak istediğiniz dizini girin: ")
+    esik_gun = float(input("Kaç günden eski dosyaları listelemek istiyorsunuz? "))
+    
+    # Ana dizin kontrolü ve derinlik sınırlaması
+    max_derinlik = 0
+    if dizin == "/":
+        max_derinlik = 3
+
     if os.path.isdir(dizin):
-        eski_dosyalari_listele(dizin)
+        bulunan_dosyalar = dosyalari_tara(dizin, max_derinlik)
+        if bulunan_dosyalar:
+            tarama_ilerlemesi(bulunan_dosyalar, len(bulunan_dosyalar))
+        else:
+            print("Belirtilen kriterlere uygun dosya veya klasör bulunamadı.")
     else:
         print("Girilen dizin mevcut değil.")
